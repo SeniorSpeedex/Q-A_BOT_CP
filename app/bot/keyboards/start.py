3ed9e73c786f799a49b9ef3b99c279d05f3da730
pnby <ryda1111111@gmail.com>
2024-10-11 19:59:39 +0300

    fixed start_keyboard

--- app/bot/keyboards/start.py
+++ app/bot/keyboards/start.py
@@ -1,7 +1,7 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-faq_button = InlineKeyboardButton(text='FAQ', callback_data='faq'),
-llm_answer_button = InlineKeyboardButton(text='Ответ от ИИ', callback_data='ai'),
+faq_button = InlineKeyboardButton(text='FAQ', callback_data='faq')
+llm_answer_button = InlineKeyboardButton(text='Ответ от ИИ', callback_data='ai')
 support_button = InlineKeyboardButton(text='Поддержка', callback_data='sap')
 menu_button = InlineKeyboardButton(text='Menu', callback_data='menu')
 
