a92e74efa629eb79223f97b5a8e7f1297ec69eb7
pnby <ryda1111111@gmail.com>
2024-10-11 20:35:25 +0300

    updated README.md

--- setup.sh
+++ setup.sh
@@ -24,4 +24,20 @@ source environments/hack/bin/activate
 sudo pip install jupyter
 sudo pip install numpy pandas scikit-learn torch transformers  # Библиотеки для ML и NLP
 
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 echo "Установка завершена."
